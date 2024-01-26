836ff76aa2c1a854160ac0d948bf0328f989d3bc
iqrarijaz788@gmail.com <iqrarijaz788@gmail.com>
2024-01-26 18:51:08 +0000

    push change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const bodyParser = require("body-parser");
 const { handleRequest } = require("./middleware");
 const app = express();
-const port = 3000;
+const port = 3001;
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(
